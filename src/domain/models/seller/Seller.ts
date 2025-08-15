54d72fd97774995676c9f45007189c0fa3b6c0e7
Michel Lima <m.macedomarques@gmail.com>
2025-08-15 20:29:20 -0300

    fix: type seller id optional

--- src/domain/models/seller/Seller.ts
+++ src/domain/models/seller/Seller.ts
@@ -1,10 +1,12 @@
 export type SellerStatus = 'active' | 'inactive' | 'cancelled';
 
 export type Seller = {
-  _id: string;
+  _id?: string;
   name: string;
   displayName: string;
   logoUrl: string;
   comissionPercentage: number;
   status: SellerStatus;
+  createdAt?: Date;
+  updatedAt?: Date;
 };
