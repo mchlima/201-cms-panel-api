e4a4b2b90bc01fe82d0202df4319a27d95082546
Michel Lima <m.macedomarques@gmail.com>
2025-08-15 20:20:36 -0300

    feat: implement domain model Seller

---
+++ src/domain/models/seller/Seller.ts
@@ -0,0 +1,10 @@
+export type SellerStatus = 'active' | 'inactive' | 'cancelled';
+
+export type Seller = {
+  _id: string;
+  name: string;
+  displayName: string;
+  logoUrl: string;
+  comissionPercentage: number;
+  status: SellerStatus;
+};
