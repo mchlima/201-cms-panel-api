ba4d02fea0aff145f52082051071ead19278f6b9
Michel Lima <m.macedomarques@gmail.com>
2025-08-17 23:04:43 -0300

    fix

--- src/data/protocols/db/user/GetUserByEmailRepository.ts
+++ src/data/protocols/db/user/GetUserByEmailRepository.ts
@@ -1,5 +1,5 @@
 import { User } from '@/domain/models/user';
 
 export interface GetUserByEmailRepository {
-  getByEmail(email: string, withPasswordHash: boolean): Promise<User | null>;
+  getByEmail(email: string, withPasswordHash?: boolean): Promise<User | null>;
 }
